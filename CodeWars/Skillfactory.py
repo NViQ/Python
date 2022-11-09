0513cc141704202a8bac2b8531acedca28804b5e
Pavel Guliukin <naveq4358@gmail.com>
2022-11-09 22:41:14 +0500

    Initial commit

--- CodeWars/Skillfactory.py
+++ CodeWars/Skillfactory.py
@@ -1,13 +1,30 @@
 import random
 import time
-a = 'abcdefghijklmnopqrstuvwxyz'
-b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-c = '0123456789'
-d = '[]{}()*\,-!?'
-all = a + b + c + d
-length = int(input('Введите длину пароля '))
-password = ''.join(random.sample(all, length))
-print('Идет генерация пароля...')
-time.sleep(3)
-print('Пароль готов ')
-print(password)
+a = "abcdefghijklmnopqrstuvwxyz"
+b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+с = "0123456789"
+d = "{}[]*/,!?"
+
+all = a + b + с + d
+# length = int(input("Enter expected password length"))
+while True:
+    try:
+        length = int(input("Enter expected password length: "))
+        if length <5:
+            print("We recommend to choose from 5 to 12 symbols")
+            time.sleep(1)
+            continue
+        elif length >15:
+            print("It`s too long password, we recommend the length about 12 symbols")
+            time.sleep(1)
+            continue
+        else:
+            password = "".join(random.sample(all, length))
+            print("Generation is going...")
+            time.sleep(2)
+            print("Your password is ready")
+            print(password)
+            break
+    except ValueError:
+        print("Please enter the integer")
+        continue
